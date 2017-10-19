f960abd21bf1a4d48396df731d9f9f3ab72fca5c
jsmet <j.smet@nextech.com>
2017-10-19 11:51:06 -0400

    Create buttons and on click handlers

--- MPV-Mobile/MPV-Mobile/MPV_Mobile/MainPage.xaml.cs
+++ MPV-Mobile/MPV-Mobile/MPV_Mobile/MainPage.xaml.cs
@@ -4,14 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using System.Windows;
 
 namespace MPV_Mobile
 {
 	public partial class MainPage : ContentPage
-	{
+	{       
+
 		public MainPage()
 		{
-			InitializeComponent();
-		}
+            InitializeComponent();            
+                        
+            var Schedule = this.FindByName<Button>("Schedule");
+            var Import = this.FindByName<Button>("Import");
+            var Message = this.FindByName<Button>("Message");
+
+            Schedule.Clicked += ScheduleClicked;
+
+            Import.Clicked += ImportClicked;
+
+            Message.Clicked += MessageClicked;
+
+            void ScheduleClicked(object sender, EventArgs e)
+            {
+                Schedule.BackgroundColor = Color.Aqua;
+            }
+
+            void ImportClicked(object sender, EventArgs e)
+            {
+                Import.BackgroundColor = Color.Teal;
+            }
+
+            void MessageClicked(object sender, EventArgs e)
+            {
+                Message.BackgroundColor = Color.DarkOrange;
+            }
+        }
 	}
 }
