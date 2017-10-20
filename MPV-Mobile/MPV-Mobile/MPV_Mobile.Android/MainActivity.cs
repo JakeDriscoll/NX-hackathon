8890b2105366f719ecc5dc93a991516b52b9bf40
Jake Driscoll <j.driscoll@nextech.com>
2017-10-20 13:29:18 -0400

    CheckPoint with Togglebutton_Toggled

--- MPV-Mobile/MPV-Mobile/MPV_Mobile.Android/MainActivity.cs
+++ MPV-Mobile/MPV-Mobile/MPV_Mobile.Android/MainActivity.cs
@@ -11,7 +11,7 @@ using Plugin.Permissions;
 
 namespace MPV_Mobile.Droid
 {
-	[Activity (Label = "MPV_Mobile", Icon = "@drawable/icon", Theme="@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity (Label = "MPV Mobile", Icon = "@drawable/icon", Theme="@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
 		protected override void OnCreate (Bundle bundle)
